916106bd82971a5c049354532f4ceb252ff2d059
Michael_l <mlamere@deltadentalwi.com>
2019-08-08 15:35:37 -0500

    Add a display for the selected hero

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,16 +1,16 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
+
 export class HerosComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heros = HEROES;
+  selectedHero: Hero;
 
   constructor() {
   }
@@ -18,4 +18,7 @@ export class HerosComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
