21cde20fb071e3f97044603683ce5f650c54ee1a
Michael_l <mlamere@deltadentalwi.com>
2019-08-12 11:00:44 -0500

    add HTTPS and in memory data

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,19 +5,23 @@ import {Observable, of} from 'rxjs';
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 import {MessageService} from './message.service';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) {
+  private herosUrl = 'api/heros';
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {
   }
 
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.herosUrl);
   }
 
   getHero(id: number): Observable<Hero> {
@@ -25,4 +29,10 @@ export class HeroService {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
+
+  private log(message: string) {
+    this.messageService.add('HeroService: ${message}');
+  }
+
+
 }
